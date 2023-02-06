8ba60726aa42f15b8e087a6190dff3dc28ddb030
random-olive <104327907+random-olive@users.noreply.github.com>
2023-02-06 17:33:36 +0900

    feat: add layout templates

--- src/templates/Layouts.tsx
+++ src/templates/Layouts.tsx
@@ -1,2 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import PATH from 'constants/routePath';
+
+export const Basic = () => {
+  return (
+    <>
+      {/*header,
+        main
+        footer*/}
+    </>
+  );
+};
+
+export const Landing = () => {
+  return <>{/*랜딩 */}</>;
+};
+
+export const Contents = () => {
+    return <>{/*컨텐츠 */}</>;
+  };
+  
