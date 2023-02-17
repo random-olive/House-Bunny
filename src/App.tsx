25610e5466fb7a085e384fbfb5b2d87ae2b1b74a
random-olive <104327907+random-olive@users.noreply.github.com>
2023-02-17 18:17:20 +0900

    feat: apply responsive resize

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import {} from 'firebase/firestore';
 import styled from 'styled-components';
-
+import { useState, useEffect } from 'react';
 import { Member, Sticky } from 'components/atoms/Buttons';
-
 
 import { BasicLayout } from 'components/templates/Layouts';
 
@@ -14,9 +13,21 @@ import { LoadingBunny } from 'components/atoms/Loading';
 const LandingPage = lazy(() => import('pages/LandingPage'));
 
 function App() {
+  const [windowSize, setWindowSize] = useState<number>(window.outerWidth);
+  const handleResize = () => {
+    setWindowSize(window.outerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <div className='App'>
+    
         <Suspense fallback={<LoadingBunny />}>
           <Routes>
             <Route element={<BasicLayout />}>
